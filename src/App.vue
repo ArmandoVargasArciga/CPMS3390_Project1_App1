ecec6d52db30feade0798c7d20a366678950e4b5
Armando <vargas.a2004@gmail.com>
2025-09-18 19:15:12 -0700

    Initial comit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Home from './views/Home.vue';
+
+
+let buttonetext = "Hello";
+function test(){
+  buttonetext = "";
+}
+</script>
+
+<template>
+<RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+let buttonetext = "Hello";
+
+</script>
+
+<template>
+<button>Hello</button>
+<router-link to="/secondPage">Hello</router-link>
+</template>
+
+<style scoped></style>
