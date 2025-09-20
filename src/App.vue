b8e48ec828eee0a34101bcc23c449a2a70f84f51
Armando <vargas.a2004@gmail.com>
2025-09-19 17:40:01 -0700

    Finished The styling

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,47 @@
 <script setup>
-import Home from './views/Home.vue';
 </script>
 
 <template>
-<RouterView />
+    <div class="container">
+        <RouterView />
+    </div>
 </template>
 
-<style scoped></style>
+<style>
+    h1{
+        color: black;
+    }
+
+    button{
+        background-color: #007bff;
+        border: black solid 1px;
+        color: black;
+        cursor: pointer;
+        padding: 7px 5px;
+        font-size: 16px;
+        border-radius: 3px;
+    }
+    body{
+        background-color: #007bff;
+        font-family: Arial Helvetica sans-serif;
+    }
+
+    button:hover{
+        background-color: #b4cce6;
+    }
+
+    .container{
+        max-width: 80%;
+        margin: 40px auto;
+        padding: 30px;
+        background-color: white;
+        border-radius: 10px;
+        border: black solid 2px;
+        align-items: center;
+    }
+
+    a, a:visited{
+        text-decoration: none;
+        color: black;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@ let buttonetext = "Hello";
 </script>
 
 <template>
-<button><router-link to="/secondPage">Hello</router-link></button>
+    <button><router-link to="/secondPage">Hello</router-link></button>
 </template>
 
 <style scoped></style>
